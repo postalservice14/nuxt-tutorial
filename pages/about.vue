789e8c0336900bfea10bbb50005bf76e0ef4bbc9
John Kelly <jkelly@carsaver.com>
2018-04-07 13:35:37 -0400

    Initial Commit

---
+++ pages/about.vue
@@ -0,0 +1,10 @@
+<template>
+  <section class="section is-medium">
+    <div class="container has-text-centered">
+      <h1 class="title">About Me</h1>
+      <p>
+        My name is John Kelly, I'm a self taught software developer based in Nashville, TN. I build modern applications for the web! I'm a technical writer, I write technical articles and tutorials for various platforms including <a href="https://scotch.io/@mezie">Scotch.io</a>.
+      </p>
+    </div>
+  </section>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,8 @@
+<template>
+  <section class="section is-large">
+    <div class="container has-text-centered">
+      <h1 class="title">Hi, I'm John Kelly</h1>
+      <h2 class="subtitle">a Software Developer based in Nashville, TN.</h2>
+    </div>
+  </section>
+</template>
